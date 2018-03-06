2ae079b03b5cbbb8d255a04e6dfb38b2b7ac6db5
Cside <cside.story@gmail.com>
2018-03-06 09:00:14 +0900

    クエリを取得するところまで

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,11 +1,20 @@
+const p = m => { console.log(m) }
+const pj = m => { console.log(JSON.stringify(m, null, '  ')) }
 
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-    if (msg.color) {
-        console.log('Receive color = ' + msg.color);
-        document.body.style.backgroundColor = msg.color;
-        sendResponse('Change color to ' + msg.color);
-    } else {
-        sendResponse('Color message is none.');
+const getQ = (): string | null => {
+    const m = location.href.match(/[?&]q=([^&]+)/)
+    if (!m) return null
+    return decodeURIComponent(m[1].replace(/\+/g, ' '))
+}
+
+(() => {
+    const q = getQ() // space で分割とかはやってない ...
+    if (!q) return
+
+    const sideBlock = document.querySelector('#rhs_block')
+    if (!sideBlock) return
+    // TODO 消すまでもない。下に追いやるとかで良いのでは。
+    while (sideBlock.firstChild) {
+        sideBlock.removeChild(sideBlock.firstChild)
     }
-});
-
+})()
