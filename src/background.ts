72ebb2ce3dc28cf3fdc38830c403e3260610e5e4
Cside <cside.story@gmail.com>
2018-03-06 13:50:00 +0900

    before using axios

--- src/background.ts
+++ src/background.ts
@@ -1,8 +1,43 @@
+// XXX /runtime は何が違う？
+import Handlebars = require('handlebars');
+import Axios = require('axios');
 
-function polling() {
-    console.log('polling');
-    setTimeout(polling, 1000 * 30);
+const Debug = true
+
+const p = m => { if (Debug) console.debug(m) }
+const j = (m): string => { return JSON.stringify(m, null, '  ') }
+const pj = m => { p(j(m)) }
+
+type Bookmark = {
+    timestamp: number
+    comment: string
+    entry: {
+        snippet: string
+        count: string
+        url: string
+        title: string
+        eid: string
+    }
+}
+type Bookmarks = {
+    bookmarks: Bookmark[]
 }
 
-polling();
+const isValidBookmarks = (res: Bookmarks): boolean => {
+    return (
+        res[0] !== undefined &&
+        res[0].entry !== undefined &&
+        res[0].url !== undefined
+    )
+}
 
+(() => {
+    const template = `
+    `;
+    const compiledTemplate = Handlebars.compile(template);
+
+    chrome.runtime.onMessage.addListener((req, sender, cb) => {
+        // 'http://b.hatena.ne.jp/Cside/search/json?q=golang',
+        return true
+    })
+})()
--- src/content_script.ts
+++ src/content_script.ts
@@ -17,4 +17,17 @@ const getQ = (): string | null => {
     while (sideBlock.firstChild) {
         sideBlock.removeChild(sideBlock.firstChild)
     }
+
+    chrome.runtime.sendMessage({}, (json: string) => {
+        sideBlock.textContent = json
+    })
+
+    // var source = '<p>Hello, my name is {{name}}. I am from {{hometown}}. I have ' +
+    //          '{{kids.length}} kids:</p>' +
+    //          '<ul>{{#kids}}<li>{{name}} is {{age}}</li>{{/kids}}</ul>';
+    // var template = Handlebars.compile(source);
+    // var data = { 'name': 'Alan', 'hometown': 'Somewhere, TX',
+    //              'kids': [{'name': 'Jimmy', 'age': '12'}, {'name': 'Sally', 'age': '4'}]};
+    // var result = template(data);
+    // sideBlock.innerHTML = result
 })()
