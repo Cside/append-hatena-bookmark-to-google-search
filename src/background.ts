d08d85547ce80c25ac25d94c2bcc512c346f1daf
Cside <cside.story@gmail.com>
2018-03-15 01:04:30 +0900

    スタイルましにした

--- src/background.ts
+++ src/background.ts
@@ -11,12 +11,12 @@ import createAxios from './utils/axios'
 const template = `
     <h2 class="hb-h2">はてなブックマークの検索結果 ({{meta.total}}件)</h2>
     {{#each bookmarks}}
-        <div class="">
+        <div class="hb-entry">
             <h3 class="hb-h3">
                 <span class=""><img src="{{entry.favicon_url}}" /></span>
                 <span class=""><a href="{{entry.url}}">{{entry.title}}</a></span>
             </h3>
-            <p>
+            <p class="hb-summary">
                 <span class="hb-count"><a href="{{entry.bookmark_url}}">{{entry.count_int}} users</a></span>
                 <span class="hb-date">{{created_ymd}}</span>
                 <span class="">{{comment}}</span>
@@ -27,17 +27,50 @@ const template = `
         </div>
     {{/each}}
     <style>
-        /* reset */
+        /* ==============================================
+           reset
+           ============================================== */
         [class^="hb-"] {
-            outline: 1px dotted pink;
-            font-family: Helvetica,Arial,Roboto,sans-serif;
-            line-height: 1.5;
-            font-size: 13px;
+            font-family: Helvetica,Arial,Roboto,sans-serif !important;
+            font-size: 12px;
             color: #333;
+            line-height: 1.4;
         }
 
+        /* ==============================================
+           debug
+           ============================================== */
+        /* [class^="hb-"] { outline: 1px dotted pink; } */
+
+        /* ==============================================
+           margin
+           ============================================== */
+        .hb-h3,
+        .hb-summary,
+        .hb-snippet {
+            margin: 7px 0 7px 0;
+        }
+        .hb-h3 {
+            text-indent: -1.5em;
+            margin-left: 1.5em;
+        }
+        .hb-summary,
+        .hb-snippet {
+            margin-left: 1.5em;
+        }
+
+        /* ==============================================
+           color && font-size && etc
+           ============================================== */
+        .hb-entry {
+            border-bottom: 1px solid #ddd;
+        }
         .hb-h2 {
             font-size: 14px;
+        }
+        .hb-h3 a {
+            font-size: 13px;
+            color: #333;
         }
         .hb-count a {
             color: #FF4166;
@@ -45,10 +78,6 @@ const template = `
         .hb-date,
         .hb-snippet {
             color: #999;
-        }
-        .hb-snippet {
-            -webkit-margin-start: 0px;
-            -webkit-margin-end: 0px;
         }
         .hb-snippet strong {
             color: #333;
